7c2d3e14b20beb7355db87420ab3369642c4fe16
isaoasis <chilecitooasis@gmail.com>
2024-06-20 10:49:36 -0300

    conexion a ver

--- init_db.py
+++ init_db.py
@@ -1,6 +1,12 @@
-# init_db.py
 from page import db, app
 
-with app.app_context():
-    db.create_all()
-    print("Database tables created.")
+def initialize_database():
+    with app.app_context():
+        try:
+            db.create_all()
+            print("Database tables created.")
+        except Exception as e:
+            print(f"Error creating database tables: {e}")
+
+if __name__ == "__main__":
+    initialize_database()
--- page/__init__.py
+++ page/__init__.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://default:NjXqClpM73Kx@ep-curly-dawn-a4hwnczb-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgres://default:NjXqClpM73Kx@ep-curly-dawn-a4hwnczb-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 login_manager = LoginManager(app)
